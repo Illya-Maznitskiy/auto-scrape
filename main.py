462d9da9662019408da6ed48180556fc2d96fe57
Illia <illia.maznitskyi@gmail.com>
2025-07-05 14:59:34 +0200

    Update main.py to save data to db

--- main.py
+++ main.py
@@ -1,35 +1,46 @@
-import io
-import json
-
+import asyncio
+from dotenv import load_dotenv
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
-from auto_ria_scraper.auto_ria_scraper.spiders.autoria import AutoriaSpider
+from database.connection import Database
 from logs.logger import logger
+from auto_ria_scraper.auto_ria_scraper.spiders.autoria import AutoriaSpider
+from database.save import save_json_to_db
+
+load_dotenv()
+
+JSON_OUTPUT_FILE = "output.json"
 
 
-def main():
-    """Start Scrapy crawler and save results to a JSON file."""
-    logger.info("Starting crawler via main()")
+def run_spider():
+    logger.info("Starting Scrapy spider...")
 
     settings = get_project_settings()
     settings.set(
         "FEEDS",
         {
-            "output.json": {
+            JSON_OUTPUT_FILE: {
                 "format": "json",
-                "overwrite": True,
                 "encoding": "utf-8",
+                "overwrite": True,
             },
         },
     )
 
     process = CrawlerProcess(settings)
     process.crawl(AutoriaSpider)
-    process.start()
+    process.start()  # Blocking call until crawling finishes
 
-    logger.info("Crawling completed.")
+    logger.info("Scrapy spider finished.")
+
+
+async def main():
+    db = Database()
+    await db.connect()  # <-- make sure this is awaited
+    await save_json_to_db("output.json", db)
+    await db.close()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
