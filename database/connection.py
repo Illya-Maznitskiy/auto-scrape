7891e8e387e34f23a9266cd58e94c79d98f2cb36
Illia <illia.maznitskyi@gmail.com>
2025-07-05 14:57:46 +0200

    Add connection.py for DB connection to database package

---
+++ database/connection.py
@@ -0,0 +1,34 @@
+import os
+
+import asyncpg
+
+from logs.logger import logger
+
+
+class Database:
+    def __init__(self):
+        self.pool = None
+
+    async def connect(self):
+        logger.info("Connecting to database...")
+        try:
+            self.pool = await asyncpg.create_pool(
+                user=os.getenv("DB_USER"),
+                password=os.getenv("DB_PASSWORD"),
+                database=os.getenv("DB_NAME"),
+                host=os.getenv("DB_HOST"),
+                port=os.getenv("DB_PORT"),
+            )
+            logger.info("Database connection pool created.")
+        except Exception as e:
+            logger.error(f"Failed to connect to database: {e}")
+            raise
+
+    async def close(self):
+        if self.pool:
+            logger.info("Closing database connection pool...")
+            await self.pool.close()
+            logger.info("Database connection pool closed.")
+
+
+db = Database()
