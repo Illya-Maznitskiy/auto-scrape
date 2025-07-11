d9acd6ffd81076b08d07f9925613db7bf8c96b55
Illia <illia.maznitskyi@gmail.com>
2025-07-11 14:41:51 +0200

    Add db_utils.py for data saving utilities

---
+++ database/db_utils.py
@@ -0,0 +1,36 @@
+from database.connection import Database
+from database.save import save_json_to_db
+from logs.logger import logger
+from database.backup_db import create_backup
+
+
+async def connect_db():
+    db = Database()
+    await db.connect()
+    logger.info("Connected to DB.")
+    return db
+
+
+async def clear_old_data(db):
+    await db.truncate_cars_table()
+    logger.info("Old records removed from DB.")
+
+
+async def save_data(db, json_file="output.json"):
+    await save_json_to_db(json_file, db)
+    logger.info("Data from JSON saved to DB.")
+
+
+async def close_db(db):
+    await db.close()
+    logger.info("Database connection closed.")
+
+
+async def run_db_tasks(json_file="output.json"):
+    db = await connect_db()
+    try:
+        await clear_old_data(db)
+        await save_data(db, json_file)
+    finally:
+        await close_db(db)
+    create_backup()
